37991b520292d0afaa2fcd09037ce734fd220530
Felix Freidman <felixfreidman@gmail.com>
2022-03-22 18:33:58 +0800

    6:33PM Commit Mobile Done Only WP left

--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -4,6 +4,9 @@ const closeAppliedForm = document.getElementById("closeAppliedForm");
 const closeForm = document.getElementById("closeForm");
 const applyFormScreen = document.getElementById("applyForm");
 const appliedFormScreen = document.getElementById("appliedForm");
+const closeAppliedFormButton = document.getElementById(
+  "closeAppliedFormButton"
+);
 
 applyBtns.forEach((button) => {
   button.addEventListener("click", () => {
@@ -16,4 +19,12 @@ closeForm.addEventListener("click", () => {
 });
 closeAppliedForm.addEventListener("click", () => {
   darkLayer.classList.toggle("js--hidden");
+  applyFormScreen.classList.toggle("js--hidden");
+  appliedFormScreen.classList.toggle("js--hidden");
+});
+closeAppliedFormButton.addEventListener("click", () => {
+  darkLayer.classList.toggle("js--hidden");
+  appliedFormScreen.classList.toggle("js--hidden");
+
+  applyFormScreen.classList.toggle("js--hidden");
 });
