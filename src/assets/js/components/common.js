804b6b60b5371fa425bc242dbb40c1b05bed7868
Felix Freidman <felixfreidman@gmail.com>
2022-04-05 23:00:24 +0800

    11:00PM Commit All Destop Done

--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -2,8 +2,8 @@ const darkLayer = document.querySelector(".dark-layer");
 const applyBtns = document.querySelectorAll(".apply-btn");
 const closeForm = document.getElementById("closeForm");
 const applyFormScreen = document.getElementById("applyForm");
-// const burgerMenu = document.querySelector(".mobile-header__burger-menu");
-// const mobileMenu = document.querySelector(".mobile-menu");
+const burgerMenu = document.querySelector(".mobile-header__burger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 const allFAQs = document.querySelectorAll(".faq-block");
 
 applyBtns.forEach((button) => {
@@ -16,17 +16,106 @@ closeForm.addEventListener("click", () => {
   darkLayer.classList.toggle("js--hidden");
 });
 
-// burgerMenu.addEventListener("click", () => {
-//   burgerMenu.classList.toggle("mobile-header__burger-menu--active");
-//   mobileMenu.classList.toggle("mobile-menu--active");
-//   document.querySelector("body").classList.toggle("disable-body");
-// });
-// $(document).ready(function () {
-//   $("#userPhone").inputmask();
-// });
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("mobile-header__burger-menu--active");
+  mobileMenu.classList.toggle("mobile-menu--active");
+  document.querySelector("body").classList.toggle("disable-body");
+});
+$(document).ready(function () {
+  $("#userPhone").inputmask();
+});
 
 allFAQs.forEach((faq) => {
   faq.addEventListener("click", () => {
     faq.classList.toggle("faq-block--active");
   });
 });
+
+window.onscroll = function () {
+  stickTheMenu();
+  unStickTheMenu();
+  activateTheListPoints();
+};
+
+const sectionList = document.getElementById("sectionList");
+const sectionMenu = document.getElementById("sectionMenu");
+const sectionAbilities = document.getElementById("toAbilities");
+// MENU POINTS
+const firstMenuPoint = document.getElementById("firstMenuPoint");
+const secondMenuPoint = document.getElementById("secondMenuPoint");
+const thirdMenuPoint = document.getElementById("thirdMenuPoint");
+const fourthMenuPoint = document.getElementById("fourthMenuPoint");
+const fifthMenuPoint = document.getElementById("fifthMenuPoint");
+// LIST POINTS
+const firstListPoint = document.getElementById("firstListPoint");
+const secondListPoint = document.getElementById("secondListPoint");
+const thirdListPoint = document.getElementById("thirdListPoint");
+const fourthListPoint = document.getElementById("fourthListPoint");
+const fifthListPoint = document.getElementById("fifthListPoint");
+let sectionListOffset = sectionList.offsetTop;
+const sectionAbilitiesOffset = sectionAbilities.offsetTop;
+// MENU POINTS OFFSETS
+const firstMenuPointOffset = firstMenuPoint.offsetTop;
+const secondMenuPointOffset = secondMenuPoint.offsetTop;
+const thirdMenuPointOffset = thirdMenuPoint.offsetTop;
+const fourthMenuPointOffset = fourthMenuPoint.offsetTop;
+const fifthMenuPointOffset = fifthMenuPoint.offsetTop;
+
+function stickTheMenu() {
+  if (window.pageYOffset >= sectionListOffset) {
+    sectionList.classList.add("sticky");
+    sectionMenu.classList.add("menuPosition");
+  } else {
+    sectionList.classList.remove("sticky");
+    sectionMenu.classList.remove("menuPosition");
+  }
+}
+
+function unStickTheMenu() {
+  if (window.pageYOffset > sectionAbilitiesOffset - 350) {
+    sectionList.classList.remove("sticky");
+    sectionMenu.classList.remove("menuPosition");
+  }
+}
+
+function activateTheListPoints() {
+  console.log(firstMenuPointOffset + " First");
+  console.log(secondMenuPointOffset + " Second");
+  console.log(thirdMenuPointOffset + " Third");
+  console.log(fourthMenuPointOffset + " Fourth");
+  console.log(fifthMenuPointOffset + " Fifth");
+  console.log(window.pageYOffset + " Page");
+  if (window.pageYOffset >= firstMenuPointOffset - 100) {
+    firstListPoint.classList.add("list-item--active");
+  } else if (window.pageYOffset <= fourthMenuPointOffset) {
+    firstListPoint.classList.remove("list-item--active");
+  }
+  if (window.pageYOffset >= fifthMenuPointOffset - 100) {
+    fifthListPoint.classList.add("list-item--active");
+  } else if (window.pageYOffset <= fifthMenuPointOffset) {
+    fifthListPoint.classList.remove("list-item--active");
+  }
+  if (window.pageYOffset >= secondMenuPointOffset - 100) {
+    secondListPoint.classList.add("list-item--active");
+  } else if (window.pageYOffset <= fourthMenuPointOffset) {
+    secondListPoint.classList.remove("list-item--active");
+  }
+  if (window.pageYOffset >= thirdMenuPointOffset - 100) {
+    thirdListPoint.classList.add("list-item--active");
+  } else if (window.pageYOffset <= fourthMenuPointOffset) {
+    thirdListPoint.classList.remove("list-item--active");
+  }
+  if (window.pageYOffset >= fourthMenuPointOffset - 100) {
+    fourthListPoint.classList.add("list-item--active");
+  } else if (window.pageYOffset <= fourthMenuPointOffset) {
+    fourthListPoint.classList.remove("list-item--active");
+  }
+}
+
+function diactivateAllListPoints() {
+  const listPoinstArray = document.querySelectorAll(".list-item");
+  console.log(listPoinstArray);
+  listPoinstArray.forEach((point) => {
+    point.classList.remove("list-item--active");
+  });
+}
